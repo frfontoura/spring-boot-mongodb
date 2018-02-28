9afc8fc5b9f5ba59a61679aa56a3fdaac9c5ce3f
unknown <frfontoura@gmail.com>
2018-02-28 19:34:18 -0300

    Update Users

--- src/main/java/com/workshopmongodb/services/UserService.java
+++ src/main/java/com/workshopmongodb/services/UserService.java
@@ -23,20 +23,31 @@ public class UserService {
 	public User findById(String id) {
 		User user = repo.findOne(id);
 		if (user == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado");
+			throw new ObjectNotFoundException("Usereto não encontrado");
 		}
 		return user;
 	}
 	
-	public User insert(User obj) {
-		return repo.insert(obj);
+	public User insert(User user) {
+		return repo.insert(user);
 	}
 	
 	public void delete(String id) {
 		repo.delete(findById(id));
 	}
 	
-	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	public User update(User user) {
+		User newUser = repo.findOne(user.getId());
+		updateData(newUser, user);
+		return repo.save(newUser);
+	}
+	
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
+	
+	public User fromDTO(UserDTO userDto) {
+		return new User(userDto.getId(), userDto.getName(), userDto.getEmail());
 	}
 }
