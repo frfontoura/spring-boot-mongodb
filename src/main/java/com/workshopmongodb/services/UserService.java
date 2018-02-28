d3ac7798d3752859127e93c361b4dfcd7b4a65af
unknown <frfontoura@gmail.com>
2018-02-28 19:09:55 -0300

    Busca de usuario por id

--- src/main/java/com/workshopmongodb/services/UserService.java
+++ src/main/java/com/workshopmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.workshopmongodb.domain.User;
 import com.workshopmongodb.repositories.UserRepository;
+import com.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,13 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		User user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
 	}
 }
