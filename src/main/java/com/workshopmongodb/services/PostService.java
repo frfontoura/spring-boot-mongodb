4b415bbf87af476c105aa2dbf94a7205c01f93e9
unknown <frfontoura@gmail.com>
2018-03-01 20:33:42 -0300

    Endpoint Post

---
+++ src/main/java/com/workshopmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.workshopmongodb.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshopmongodb.domain.Post;
+import com.workshopmongodb.repositories.PostRepository;
+import com.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Post post = postRepository.findOne(id);
+		if (post == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return post;
+	}
+}
